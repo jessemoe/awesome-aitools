036dda46f472f111e1f2a62f84cad61af452225c
stark-eth <1319114370@qq.com>
2023-03-23 00:15:41 +0800

    update paint tools

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Contact from '@/components/Contact';
 import Icon from '@/components/Icon';
 import { AiFillFileText, AiOutlineVideoCameraAdd } from 'react-icons/ai';
 import { SiOpenai, SiNotion, SiAirplayaudio } from 'react-icons/si'
-import {GiLargePaintBrush} from 'react-icons/gi'
+import { GiLargePaintBrush } from 'react-icons/gi'
 
 export default function About() {
   const TEXT_TOOLS = [{ icon: SiOpenai, href: 'https://chat.openai.com/chat', label: 'Chatgpt' },
@@ -11,11 +11,15 @@ export default function About() {
   { icon: '/img/writesonic.jpeg', href: 'https://writesonic.com', label: 'Writesonic' },
   { icon: '/img/copyai.jpeg', href: 'https://www.copy.ai', label: 'Copy.ai' },
   ]
-  const PAINT_TOOLS = [{ icon: '/img/midjourney.jpeg', href: 'https://www.midjourney.com', label: 'Midjourney' },
-  { icon: '/img/photorroom.jpeg', href: 'https://www.photoroom.com', label: 'PhotorRoom' },
-  { icon: '/img/printid.png', href: 'https://www.printidea.art/', label: '造梦师' },
-  { icon: '/img/arc.jpeg', href: 'https://arc.tencent.com', label: 'ARC Lab' },
-  { icon: '/img/artbreed.jpeg', href: 'artbreeder.com/', label: 'Artbreeder' },
+  const PAINT_TOOLS = [
+    { icon: '/img/paint.png', href: 'https://stablediffusionweb.com/', label: 'Stable Diffusion' },
+    { icon: '/img/paint.png', href: 'https://github.com/cloneofsimo/lora', label: 'LoRA' },
+    { icon: '/img/midjourney.jpeg', href: 'https://www.midjourney.com', label: 'Midjourney' },
+    { icon: '/img/photorroom.jpeg', href: 'https://www.photoroom.com', label: 'PhotorRoom' },
+    { icon: '/img/novelai.jpeg', href: 'https://novelai.net/', label: 'NovelAI' },
+    { icon: '/img/printid.png', href: 'https://www.printidea.art/', label: '造梦师' },
+    { icon: '/img/arc.jpeg', href: 'https://arc.tencent.com', label: 'ARC Lab' },
+    { icon: '/img/artbreed.jpeg', href: 'artbreeder.com/', label: 'Artbreeder' },
   ]
   const AUDIO_TOOLS = [{ icon: '/img/brainfm.jpeg', href: 'https://www.brain.fm', label: 'Brain.fm' },
   { icon: '/img/soundraw.jpeg', href: 'https://soundraw.io', label: 'Soundraw' },
